e4527e90f8b4740062a97d1dd09108c318a455b1
theThirdEye <n.mitsume@gmail.com>
2012-07-07 00:10:18 +0000

    add okinawan sound

--- canvas.js
+++ canvas.js
@@ -1,7 +1,10 @@
 var ctx;
 var canvas;
 var pad;
-var PAD_NUM = 9;
+var PAD_NUM = 27;
+
+
+
 
 /********************************KeyPadのクラス*************************************/
 var Keypad = function(bufNum, x, y, width, height, r, g, b, key) {
@@ -13,12 +16,17 @@ var Keypad = function(bufNum, x, y, width, height, r, g, b, key) {
 	this.color  = "rgba("+r+","+g+","+b+",";
 	this.bufNum = bufNum;
 	this.key = key;
+
+	this.brightness;
+	this.lighting = 0;
 };
 
 //描画メソッド
 Keypad.prototype.draw = function() {
 	ctx.beginPath();
-	ctx.fillStyle = this.color + "0.3)";
+	ctx.clearRect(this.x, this.y, this.width, this.height);
+	this.brightness = 0.2 + 0.8*(this.lighting/15);
+	ctx.fillStyle = this.color + this.brightness +")";
 	ctx.fillRect(this.x, this.y, this.width, this.height);
 	ctx.strokeStyle = this.color + "0.9)";
 	ctx.strokeRect(this.x, this.y, this.width, this.height);
@@ -49,6 +57,7 @@ Keypad.prototype.isInside = function(x0, y0) {
 
 //padが押されたときの動作
 Keypad.prototype.push = function() {
+	this.lighting = 15;
 	wap_playSound(this.bufNum);
 };
 /*********************************************************************************/
@@ -60,24 +69,59 @@ function canvas_init() {
 	canvas = document.getElementById("canvas1");
 	ctx    = canvas.getContext("2d");
 	
+	setInterval("lighting()", 50);	
+	
 	pad = new Array(PAD_NUM);
-	pad[0] = new Keypad(0, 150, 400, 80, 80, 255, 0, 0, "c");
-	pad[1] = new Keypad(1, 250, 400, 80, 80, 255, 0, 0, "v");
-	pad[2] = new Keypad(2, 350, 400, 80, 80, 255, 0, 0, "b");
-	pad[3] = new Keypad(3, 450, 400, 80, 80, 255, 0, 0, "n");
-	pad[4] = new Keypad(4, 550, 400, 80, 80, 255, 0, 0, "m");
-	pad[5] = new Keypad(5, 200, 300, 80, 80, 255, 0, 0, "f");
-	pad[6] = new Keypad(6, 300, 300, 80, 80, 255, 0, 0, "g");
-	pad[7] = new Keypad(7, 400, 300, 80, 80, 255, 0, 0, "h");
-	pad[8] = new Keypad(8, 500, 300, 80, 80, 255, 0, 0, "j");
+	pad[ 0] = new Keypad(28,  10, 250, 60, 60, 255, 0, 0, "q");
+	pad[ 1] = new Keypad(29,  90, 250, 60, 60, 255, 0, 0, "w");
+	pad[ 2] = new Keypad(30, 170, 250, 60, 60, 255, 0, 0, "e");
+	pad[ 3] = new Keypad(31, 250, 250, 60, 60, 255, 0, 0, "r");
+	pad[ 4] = new Keypad(32, 330, 250, 60, 60, 255, 0, 0, "t");
+	pad[ 5] = new Keypad(9, 410, 250, 60, 60, 80, 80, 80, "y");
+	pad[ 6] = new Keypad(9, 490, 250, 60, 60, 80, 80, 80, "u");
+	pad[ 7] = new Keypad(15, 570, 250, 60, 60, 0, 255, 0, "i");
+	pad[ 8] = new Keypad(16, 650, 250, 60, 60, 0, 255, 0, "o");
+	pad[ 9] = new Keypad(17, 730, 250, 60, 60, 0, 255, 0, "p");
+	
+	pad[10] = new Keypad(23,  50, 330, 60, 60, 255, 0, 0, "a");
+	pad[11] = new Keypad(24, 130, 330, 60, 60, 255, 0, 0, "s");
+	pad[12] = new Keypad(25, 210, 330, 60, 60, 255, 0, 0, "d");
+	pad[13] = new Keypad(26, 290, 330, 60, 60, 255, 0, 0, "f");
+	pad[14] = new Keypad(27, 370, 330, 60, 60, 255, 0, 0, "g");
+	pad[15] = new Keypad(9, 450, 330, 60, 60, 80, 80, 80, "h");
+	pad[16] = new Keypad(12, 530, 330, 60, 60, 0, 0, 255, "j");
+	pad[17] = new Keypad(13, 610, 330, 60, 60, 0, 0, 255, "k");
+	pad[18] = new Keypad(14, 690, 330, 60, 60, 0, 0, 255, "l");
+
+	pad[19] = new Keypad(18,  90, 410, 60, 60, 255, 0, 0, "z");
+	pad[20] = new Keypad(19, 170, 410, 60, 60, 255, 0, 0, "x");
+	pad[21] = new Keypad(20, 250, 410, 60, 60, 255, 0, 0, "c");
+	pad[22] = new Keypad(21, 330, 410, 60, 60, 255, 0, 0, "v");
+	pad[23] = new Keypad(22, 410, 410, 60, 60, 255, 0, 0, "b");
+	pad[24] = new Keypad( 9, 490, 410, 60, 60, 0, 0, 255, "n");
+	pad[25] = new Keypad(10, 570, 410, 60, 60, 0, 0, 255, "m");
+	pad[26] = new Keypad(11, 650, 410, 60, 60, 0, 0, 255, ",");
+
+	
 	drawAllPads();
 	
 }
 
 
 
+function lighting() {
+	for(var i=0; i<PAD_NUM; i++) {
+		if(pad[i].lighting != 0){
+			pad[i].lighting--;
+			pad[i].draw();
+		}
+	}
+}
+
+
+
 function drawAllPads() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	//ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawCanvasStroke();
 
 	for(var i=0; i<PAD_NUM; i++) {
--- mouse.js
+++ mouse.js
@@ -56,15 +56,36 @@ var getMousePos = function(e) {
 document.onkeydown = Keyboard;
 function Keyboard(evt) {
 	switch(evt.which) {
-		case 67: pad[0].push(); break;
-		case 86: pad[1].push(); break;
-		case 66: pad[2].push(); break;
-		case 78: pad[3].push(); break;
-		case 77: pad[4].push(); break;
-		case 70: pad[5].push(); break;
-		case 71: pad[6].push(); break;
-		case 72: pad[7].push(); break;
-		case 74: pad[8].push(); break;
+		case 81: pad[0].push(); break;
+		case 87: pad[1].push(); break;
+		case 69: pad[2].push(); break;
+		case 82: pad[3].push(); break;
+		case 84: pad[4].push(); break;
+		//case 89: pad[5].push(); break;
+		//case 85: pad[6].push(); break;
+		case 73: pad[7].push(); break;
+		case 79: pad[8].push(); break;
+		case 80: pad[9].push(); break;
+
+		case 65: pad[10].push(); break;
+		case 83: pad[11].push(); break;
+		case 68: pad[12].push(); break;
+		case 70: pad[13].push(); break;
+		case 71: pad[14].push(); break;
+		//case 72: pad[15].push(); break;
+		case 74: pad[16].push(); break;
+		case 75: pad[17].push(); break;
+		case 76: pad[18].push(); break;
+	
+		case 90: pad[19].push(); break;
+		case 88: pad[20].push(); break;
+		case 67: pad[21].push(); break;
+		case 86: pad[22].push(); break;
+		case 66: pad[23].push(); break;
+		case 78: pad[24].push(); break;
+		case 77: pad[25].push(); break;
+		case 188: pad[26].push(); break;
+		
 		default: break;
 	}
 }
